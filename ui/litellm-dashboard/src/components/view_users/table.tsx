d0ec7b7e7ccbf88b1d5726d880f50ff74a33bdb1
Nitish <nitish0399@hotmail.com>
2025-08-22 21:34:14 +0530

    remove litellm icons

--- ui/litellm-dashboard/src/components/view_users/table.tsx
+++ ui/litellm-dashboard/src/components/view_users/table.tsx
@@ -219,7 +219,7 @@ export function UserDataTable({
               <TableRow>
                 <TableCell colSpan={columns.length} className="h-8 text-center">
                   <div className="text-center text-gray-500">
-                    <p>🚅 Loading users...</p>
+                    <p>Loading users...</p>
                   </div>
                 </TableCell>
               </TableRow>
